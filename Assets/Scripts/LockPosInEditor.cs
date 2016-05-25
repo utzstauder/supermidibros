678aed6c4078d8f0f4268b75b887fdc732e7f018
Utz Stauder <us@colognegamelab.de>
2016-05-25 20:40:43 +0200

    Version 0.1
    Imported MidiJack plugin
    Created a test scene which serves as a proof-of-concept for the MIDI
    input

---
+++ Assets/Scripts/LockPosInEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LockPosInEditor : MonoBehaviour {
+
+	public bool lockX = false;
+	public bool lockY = false;
+	public bool lockZ = false;
+
+	public Vector3 coordinates = Vector3.zero;
+
+	public bool snapX = false;
+	public bool snapY = false;
+	public bool snapZ = false;
+
+	public Vector3 snapInterval = Vector3.zero;
+	public Vector3 snapOffset = Vector3.zero;
+
+	private float newX = 0;
+	private float newY = 0;
+	private float newZ = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (!Application.isPlaying){
+			//
+			if (snapX) lockX = false;
+			if (snapY) lockY = false;
+			if (snapZ) lockZ = false;
+
+			// calculate new coordinates
+			if (lockX) newX = coordinates.x;
+			else if (snapX) newX = Mathf.RoundToInt(this.transform.position.x / snapInterval.x) * snapInterval.x + snapOffset.x;
+			else newX = this.transform.position.x;
+
+			if (lockY) newY = coordinates.y;
+			else if (snapY) newY = Mathf.RoundToInt(this.transform.position.y / snapInterval.y) * snapInterval.y + snapOffset.y;
+			else newY = this.transform.position.y;
+
+			if (lockZ) newZ = coordinates.z;
+			else if (snapZ) newZ = Mathf.RoundToInt(this.transform.position.z / snapInterval.z) * snapInterval.z + snapOffset.z;
+			else newZ = this.transform.position.z;
+
+			this.transform.position = new Vector3(newX, newY, newZ);
+		}
+	}
+}
---
+++ Assets/Scripts/TransformWithVelocity.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using MidiJack;
+
+public class TransformWithVelocity : MonoBehaviour {
+
+	public Vector3 translate = Vector3.zero;
+	public Vector3 rotate = Vector3.zero;
+	public Vector3 scale = Vector3.zero;
+
+	public int noteNumber = 0;
+
+	private Vector3 initialLocalPosition;
+	private Vector3 initialLocalRotation;
+	private Vector3 initialLocalScale;
+
+	// Use this for initialization
+	void Awake () {
+		initialLocalPosition = transform.localPosition;
+		initialLocalRotation = transform.localRotation.eulerAngles;
+		initialLocalScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = initialLocalPosition + (new Vector3(translate.x, translate.y, translate.z) * MidiMaster.GetKey(noteNumber));
+		transform.localRotation = Quaternion.Euler(initialLocalRotation + (new Vector3(rotate.x, rotate.y, rotate.z) * MidiMaster.GetKey(noteNumber)));
+		transform.localScale = initialLocalScale + (new Vector3(scale.x, scale.y, scale.z) * MidiMaster.GetKey(noteNumber));
+	}
+}
